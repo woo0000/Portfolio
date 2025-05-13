d6e0e64eea11e0bee50150422858847089f66055
woo0000 <woo04031031@gmail.com>
2025-05-13 14:45:50 +0900

    2025-05-13-03

--- js/lenis.js
+++ js/lenis.js
@@ -1,6 +1,9 @@
 function lenisAnimation(){
 	const lenis=new Lenis({
 		duration: 2,
+		smooth: true,
+		smoothTouch: true,
+		direction: 'vertical',
 		easing: t => Math.min(1, 1.001-Math.pow(2, -10*t))
 	});
 
